94e2221a8ba452aa5bf009260ed82374d67748fd
nicoleHTW <nicole.meier@iw.htwchur.ch>
2015-11-30 10:29:49 +0100

    neueAufgabe.php

---
+++ www/api/neueAufgabe.php
@@ -0,0 +1,33 @@
+<?php
+
+
+$nachname=$_GET['nachname'];
+$vorname=$_GET['vorname'];
+
+try {
+    require_once 'dbConnect.php';
+    $sql = "INSERT INTO dozent (idDozent, nachname, vorname) VALUES (default, '{$nachname}', '{$vorname}')";
+    $result = $datenbank->query($sql);
+    echo 'Rows affected: ' . $datenbank->affected_rows . '<br>';
+    echo $result . '<br>';
+    if($result){
+        // $result ist 1 wenn das insert statement ausgeführt wurde, sonst ist es leer
+        $response=array("fehlercode"=>"1","botschaft"=>"Benutzer gespeichert!");
+    } else {
+        $response=array("fehlercode"=>"0","botschaft"=>"Benutzer nicht gespeichert, existiert schon.");
+    }
+    //var_dump($response);
+    // echo '<pre>';
+    // print_r($response);
+    // echo '</pre>';
+    echo json_encode($response);
+} catch (Exception $e) {
+    $error = $e->getMessage();
+}
+if (isset($error)) {
+    echo "<p>$error</p>";
+}
+$datenbank->close();
+
+
+?>
